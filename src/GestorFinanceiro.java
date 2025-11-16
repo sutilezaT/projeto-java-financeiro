ef44b96d1a34ffee6abd8871117b4829af1bf459
Thaís Sutil <thaissutil.dev@gmail.com>
2025-11-16 09:40:00 -0300

    Upgrade v0.1: Adiciona Scanner para inputs dinâmicos

--- src/GestorFinanceiro.java
+++ src/GestorFinanceiro.java
@@ -1,20 +1,37 @@
+import java.util.Scanner;
+
 public class GestorFinanceiro {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("\n=== Meu Gestor de Finanças ===");
 
-        double despesa1 = 50.0;
-        double despesa2 = 85.5;
-        double despesa3 = 138.9;
+        System.out.println("Salário mensal: R$ ");
+        double salarioMensal = scanner.nextDouble();
 
-        double salarioMensal = 2385.14;
+        System.out.println("valor da despesa 1: R$ ");
+        double despesa1 = scanner.nextDouble();
+
+        System.out.println("Valor da despesa 2: R$ ");
+        double despesa2 = scanner.nextDouble();
+
+        System.out.println("Valor da despes 3: R$ ");
+        double despesa3 = scanner.nextDouble();
 
         double totalDespesas = despesa1 + despesa2 + despesa3;
 
         double saldoRestante = salarioMensal - totalDespesas;
 
-        System.out.println("Salário Mensal: R$ " + salarioMensal);
-        System.out.println("Total de Despesas: R$ " + totalDespesas);
-        System.out.println("Total do Saldo Restante: R$ " + saldoRestante);
+        if (saldoRestante < 0) {
+            System.out.println("\n===ATENÇÃO!=== \nSeu saldo está negativo!");
+            System.out.printf("Saldo Restante: R$ %.2f", saldoRestante);
+
+        } else {
+            System.out.println("\nSalário Mensal: R$ " + salarioMensal);
+            System.out.printf("Total do Saldo Restante: R$ R$ %.2f\n", saldoRestante);
+        }
+
+        scanner.close();
     }
 }
